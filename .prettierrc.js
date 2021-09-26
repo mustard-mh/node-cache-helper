9350382c8740618291e17b22fdf5579e1da478d5
hwen <mhqnwt@gmail.com>
2021-09-26 17:52:55 +0800

    Cache helper

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "avoid",
+  overrides: [
+    {
+      files: "*.js",
+      options: {
+        semi: false,
+        singleQuote: true,
+      },
+    },
+  ],
+};
---
+++ index.d.ts
@@ -0,0 +1,14 @@
+import * as NodeCache from "node-cache";
+import { Options } from "node-cache";
+
+export declare class NodeCacheHelper {
+  instance: NodeCache;
+  registerMap: Record<string, Function>;
+
+  constructor(config: Options);
+  register(prefix: string, reloadFn: Function);
+  resetKey(key: string): Promise<any>;
+  get(key: string): Promise<any>;
+}
+
+export = NodeCacheHelper;
